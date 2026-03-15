7df5ed762f2432c66422962f4968ee59dce2908c
Marco Andres Silva <marco.silvaponce10@gmail.com>
2026-03-14 23:12:41 -0300

    feat: set environment to deploy in render update

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,15 +10,16 @@ import { ConfigModule } from '@nestjs/config';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: '.env',
     }),
 
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: parseInt(process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USERNAME,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE,
+      host: process.env.POSTGRES_HOST || 'localhost',
+      port: Number(process.env.POSTGRES_PORT) || 5432,
+      username: process.env.POSTGRES_USERNAME || 'postgres',
+      password: process.env.POSTGRES_PASSWORD || 'postgres',
+      database: process.env.POSTGRES_DATABASE || 'postgres',
       autoLoadEntities: true,
       synchronize: true,
     }),
