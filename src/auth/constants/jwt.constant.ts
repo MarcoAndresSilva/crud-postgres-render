2434800932542415f6356cfc0f642611ee6412be
Marco Andres Silva <marco.silvaponce10@gmail.com>
2026-03-17 01:36:30 -0300

    .env jwt

--- src/auth/constants/jwt.constant.ts
+++ src/auth/constants/jwt.constant.ts
@@ -1,3 +1,4 @@
+console.log(process.env.JWT_SECRET);
 export const jwtConstants = {
-  secret: 'no utilizar esta palabra en producción',
+  secret: process.env.JWT_SECRET || 'default_secret_key',
 };
